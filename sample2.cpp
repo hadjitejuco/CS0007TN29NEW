d895a3526f13dd59b0378941da36295788a3e03a
Hadji J. Tejuco <hadji.tejuco@gmail.com>
2024-01-03 11:30:42 +0800

    Add files via upload

---
+++ sample2.cpp
@@ -0,0 +1,14 @@
+//https://github.com/hadjitejuco/CS0007TN29NEW
+//default value 
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num[10]={10,20,30,40,50}; //array
+	int i; //loop
+	for(i=0;i<10;i++)
+	{
+		cout << i+1 << "::" << num[i]<< endl;
+	}
+	return 0;
+}
